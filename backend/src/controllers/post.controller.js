cd15856cbd0f1329a4b86fe64e3b82c5fc0a71ec
roy <roypolocarrillo@gmail.com>
2023-10-08 20:47:19 -0500

    act

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,15 +1,14 @@
 import { pool } from "../db.js";
 
-export const postImg = async(req, res) => {
-  const {ruta_img} = req.body;
-
-  
-}
-
+// ruta para subir productos con imagenes 
 export const postProduct = async (req, res) => {
   try {
+    const file = req.file
+    const imagen = {
+      name: file.originalname
+    }
     const {id_producto, nomb_producto, descripcion, precio} = req.body;
-    const [row] = await pool.query("INSERT INTO productos (id_producto, nomb_producto, descripcion, precio) VALUES (?,?,?,?)",[ id_producto, nomb_producto, descripcion, precio])
+    const [row] = await pool.query("INSERT INTO productos (id_producto, nomb_producto, descripcion, precio, ruta_img) VALUES (?,?,?,?,?)",[ id_producto, nomb_producto, descripcion, precio, imagen.name])
     res.send(row)
   } catch (error) {
     console.log(error)
