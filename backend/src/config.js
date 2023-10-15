7fa2aba15bee850de99184a0fbbfe206db7983d2
roy <roypolocarrillo@gmail.com>
2023-10-14 21:44:39 -0500

    act

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,8 +3,9 @@ import  {config}  from "dotenv"
 config();
 
 export const PORT_SERVER = process.env.PORT_SERVER;
-export const DB_HOST = process.env.DB_HOST 
+export const DB_HOST = process.env.DB_HOST; 
 export const DB_PORT =process.env.DB_PORT;
-export const DB_USER = process.env.DB_USER ;
-export const DB_PASSWORD = process.env.DB_PASSWORD ;
-export const DB_DATABASE = process.env.DB_DATABASE ;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_DATABASE = process.env.DB_DATABASE;
+export const SECRET_KEY = process.env.SECRET_KEY
--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,5 +1,7 @@
 import { pool } from "../db.js";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "../config.js";
 // ruta para subir productos con imagenes 
 export const postProduct = async (req, res) => {
   try {
@@ -14,4 +16,31 @@ export const postProduct = async (req, res) => {
     console.log(error)
   }
 }
-
+//Query para inicio de sesion
+export const PostLogin = async (req, res) => {
+  try {
+    const {correo, contraseña} = req.body;
+    const [Login] = await pool.query('SELECT correo, contraseña rol.rol FROM Login INNER JOIN rol ON Login.rol = rol.rol WHERE correo = ?',[correo]);
+    if(Login.affectedRows>0){
+      const comparePassword = await bcrypt.compare(contraseña, Login[0].contraseña);
+      if(comparePassword){
+        const token = jwt.sing({
+          id: Login[0].id,
+          correo: Login[0].correo
+        },
+        SECRET_KEY,
+        {
+          expireIn: "1h",
+        });
+        res.status(200).json({token})
+      }else{
+        return res.status(400).json({msg:"Contraseña incorrecta"});
+      }
+    }else{
+      return res.status(400).json({msg:"Usuario no existe"});
+    }
+  } catch (error) {
+    res.status(500).json({error: error })
+    
+  }
+}
