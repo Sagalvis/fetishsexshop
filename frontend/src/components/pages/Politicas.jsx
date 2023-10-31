8efa7d197027404f696c09c50f91730b596868f0
beecodes <jdiaz@ulearning.com.co>
2023-10-30 22:13:56 -0500

    Vistas de nosotros y politicas terminadas un 80%

--- frontend/src/components/pages/Politicas.jsx
+++ frontend/src/components/pages/Politicas.jsx
@@ -1,28 +1,47 @@
-import styled from "styled-components";
 import Footer from "../Footer";
 import Header from "../Header";
+import {
+  ContentMain,
+  ContentShop,
+  ContentText,
+  Hh2,
+  Img,
+  Text,
+} from "./styles/styledPoliticas";
+import image1 from "../../assets/politicas/7-dias.png";
+// import image2 from '../../assets/nosotros/sex-shop2.png'     //en espera.
 
 const Politicas = () => {
   return (
     <>
       <Header />
-        <ContainerN>
-          VISTA EN CONSTRUCCÍON 
-        </ContainerN>
+
+      <ContentMain>
+        <ContentShop>
+          <Img src={image1} alt="image" />
+        </ContentShop>
+
+        <ContentText>
+          <Hh2>Politicas de devolución</Hh2>
+          <Text>
+            Realizamos la entrega de nuestros productos con el adecuado embalaje
+            para protegerlo durante la cadena de distribución. <br />Es por esto que
+            ofrecemos el servicio de devolución de productos. Motivos de
+            devolución. <br />
+            - Que el producto alcance su fecha de vencimiento. <br />
+            - Que el embalaje sea visiblemente vulnerado. <br />
+            - Que el producto no sea el mismo que usted haya ordenado, una vez pasados 7 días después de la entrega no se aceptan devoluciones. <br /> <br />
+            Para el servicio de devoluciones
+            debe comunicarse con nuestros asesores a través de WhatsApp o a
+            través de nuestro correo electrónico ###########, se analizará el
+            caso y se darán instrucciones.
+          </Text>
+        </ContentText>
+      </ContentMain>
+
       <Footer />
     </>
   );
 };
 
 export default Politicas;
-
-const ContainerN = styled.div`
-  width: 100%;
-  height: 64.5%;
-  background-color: #1b1b1b;
-  color: white;
-  font-size: 3rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
---
+++ frontend/src/components/pages/styles/styledNosotros.jsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const ContentMain = styled.div`
+  /* background-color: green; */
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  box-sizing: border-box;
+`;
+
+export const ContentShop = styled.div`
+  /* background-color: red; */
+  width: 50%;
+  height: 85vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+`;
+
+export const Img = styled.img`
+  width: 270px;
+  
+`;
+
+export const ContentText = styled.div`
+  /* background-color: green; */
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  padding: 25px;
+  width: 100%;
+`;
+
+export const Hh2 = styled.h2`
+`;
+
+export const Span = styled.span`
+  font-weight: 600;
+`;
+
+export const Text = styled.p`
+  text-align: justify;
+`;
